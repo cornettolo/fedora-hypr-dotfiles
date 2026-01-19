91142acaf902896a311491f638a4a93005d8ae90
cornettolo <giacoandre94@gmail.com>
2026-01-19 09:44:19 +0100

    bigger fonts

---
+++ config/nvim/lua/plugins/surround.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "echasnovski/mini.surround",
+    vscode = true,
+    opts = {
+      mappings = {
+        add = "ys",
+        delete = "ds",
+        find = "",
+        find_left = "",
+        highlight = "",
+        replace = "cs",
+        update_n_lines = "",
+      },
+    },
+  },
+}
